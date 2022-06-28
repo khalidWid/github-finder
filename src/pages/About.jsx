9a8644c2b0418c4be90931cc7a4df9a6eef6f400
khalid id-ouarab <khalid.idouarab@gmail.com>
2022-06-28 19:27:56 +0100

    commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4 text-white">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light ">
+        A react app to search Github profiles and see profile details.
+      </p>
+      <p className="text-lg text-gray-400 ">
+        Layout By:
+        <a href="https://github.com/khalidWid" className="text-white ml-2">
+          khalidWid
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default About;
